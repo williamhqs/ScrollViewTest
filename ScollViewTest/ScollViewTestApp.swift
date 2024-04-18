d1dcb839f159a60c34cb86996551967bc969bd59
Huqiushi <william.hqs@gmail.com>
2024-04-18 10:37:08 +0800

    Initial Commit

---
+++ ScollViewTest/ScollViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScollViewTestApp.swift
+//  ScollViewTest
+//
+//  Created by Hu Qiushi on 2024/4/18.
+//
+
+import SwiftUI
+
+@main
+struct ScollViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
